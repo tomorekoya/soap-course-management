b21d0083f6600a5bbf2b5e7c406c181df6f43944
Tomiwa Orekoya <orekoyatomiwa@gmail.com>
2022-05-09 17:37:42 -0400

    Created an Endpoint class for handling course detail requests.

---
+++ src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -0,0 +1,30 @@
+package com.in28minutes.soap.webservices.soapcoursemanagement.soap;
+
+import com.in28minutes.courses.CourseDetails;
+import com.in28minutes.courses.GetCourseDetailsRequest;
+import com.in28minutes.courses.GetCourseDetailsResponse;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CourseDetailsEndpoint {
+
+    // method
+    // input: GetCourseDetailsRequest
+    // output: GetCourseDetailsResponse
+    // namespace: http://in28minutes.com/courses
+    // request type: GetCourseDetailsRequest
+    @PayloadRoot(namespace = "http://in28minutes.com/courses", localPart = "GetCourseDetailsRequest")
+    @ResponsePayload
+    public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
+        GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+        CourseDetails courseDetails = new CourseDetails();
+        courseDetails.setId(request.getId());
+        courseDetails.setName("Microservices Course");
+        courseDetails.setDescription("That would be a wonderful course!");
+        response.setCourseDetails(courseDetails);
+        return response;
+    }
+}
