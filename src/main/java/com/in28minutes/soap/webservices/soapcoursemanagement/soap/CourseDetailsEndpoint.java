0c7c2a4e96e5d5ec61a3926438843419702b6c6c
Tomiwa Orekoya <orekoyatomiwa@gmail.com>
2022-05-09 19:41:44 -0400

    Refactored the course endpoint to get data from a service.

--- src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
+++ src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -3,6 +3,9 @@ package com.in28minutes.soap.webservices.soapcoursemanagement.soap;
 import com.in28minutes.courses.CourseDetails;
 import com.in28minutes.courses.GetCourseDetailsRequest;
 import com.in28minutes.courses.GetCourseDetailsResponse;
+import com.in28minutes.soap.webservices.soapcoursemanagement.soap.bean.Course;
+import com.in28minutes.soap.webservices.soapcoursemanagement.soap.service.CourseDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -10,6 +13,9 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class CourseDetailsEndpoint {
+
+    @Autowired
+    CourseDetailsService service;
 
     // method
     // input: GetCourseDetailsRequest
@@ -19,11 +25,17 @@ public class CourseDetailsEndpoint {
     @PayloadRoot(namespace = "http://in28minutes.com/courses", localPart = "GetCourseDetailsRequest")
     @ResponsePayload
     public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
+        Course course = service.findById(request.getId());
+        return mapCourse(course);
+    }
+
+    private GetCourseDetailsResponse mapCourse(Course course) {
         GetCourseDetailsResponse response = new GetCourseDetailsResponse();
         CourseDetails courseDetails = new CourseDetails();
-        courseDetails.setId(request.getId());
-        courseDetails.setName("Microservices Course");
-        courseDetails.setDescription("That would be a wonderful course!");
+
+        courseDetails.setId(course.getId());
+        courseDetails.setName(course.getName());
+        courseDetails.setDescription(course.getDescription());
         response.setCourseDetails(courseDetails);
         return response;
     }
