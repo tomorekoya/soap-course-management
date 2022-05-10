29209f94167d10d12392958ffe29ee4362d6d994
Tomiwa Orekoya <orekoyatomiwa@gmail.com>
2022-05-10 09:37:02 -0400

    Added a SecurityInterceptor using XwsSecurity.
    - Checks for a username and password in the header of a request.

--- src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/soap/WebServiceConfig.java
+++ src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/soap/WebServiceConfig.java
@@ -6,16 +6,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
+import org.springframework.ws.soap.security.xwss.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+import javax.security.auth.callback.CallbackHandler;
+import java.util.Collections;
+import java.util.List;
+
 // Enable Spring web services
 @EnableWs
 // Spring configuration
 @Configuration
-public class WebServiceConfig {
+public class WebServiceConfig extends WsConfigurerAdapter {
     // MessageDispatcherServlet
     // @param ApplicationContext
     // endpoint: url -> /ws/*
@@ -46,5 +54,26 @@ public class WebServiceConfig {
     @Bean
     public XsdSchema coursesSchema() {
         return new SimpleXsdSchema(new ClassPathResource("course-details.xsd"));
+    }
+
+    @Bean
+    public XwsSecurityInterceptor securityInterceptor() {
+        XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
+        securityInterceptor.setCallbackHandler(getCallbackHandler());
+        securityInterceptor.setPolicyConfiguration(new ClassPathResource("securityPolicy.xml"));
+        // Callback Handler -> SimplePasswordValidationCallbackHandler
+        // Security Policy -> securityPolicy.xml
+        return securityInterceptor;
+    }
+
+    private SimplePasswordValidationCallbackHandler getCallbackHandler() {
+        SimplePasswordValidationCallbackHandler handler = new SimplePasswordValidationCallbackHandler();
+        handler.setUsersMap(Collections.singletonMap("user", "password"));
+        return handler;
+    }
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(securityInterceptor());
     }
 }
